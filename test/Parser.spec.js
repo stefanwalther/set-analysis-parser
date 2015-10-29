b8a07e49549ce46d7a12fe7d4534413a2bb51094
Stefan Walther <swr@qlik.com>
2015-10-29 18:26:19 +0100

    Support for negative tests.

--- test/Parser.spec.js
+++ test/Parser.spec.js
@@ -4,13 +4,21 @@ import _ from 'lodash';
 
 let grammar = fs.readFileSync( './src/parser.jison', 'utf8' );
 let expressions = fs.readFileSync( './test/fixtures/expressions.txt', 'utf8' ).split( '\n' );
-expressions = expressions.filter( function ( line ) {
-	return !_.startsWith( line, 'x' ) && !_.startsWith(line, '#');
+let positiveTests = fs.readFileSync( './test/fixtures/expressions.txt', 'utf8' ).split( '\n' );
+positiveTests = expressions.filter( function ( line ) {
+	return !_.startsWith( line, 'x' ) && !_.startsWith(line, '#') && !_.startsWith(line, 'f');
 } );
+
+let negativeTests = [];
+expressions.forEach( function ( line ) {
+	if (_.startsWith(line, 'f')) {
+		negativeTests.push(_.trimLeft(line, 'f'));
+	}
+});
 let parser = new Parser( grammar );
 let parserSource = parser.generate();
 
-describe.only( 'Parser', () => {
+describe( 'Parser', () => {
 
 	it( 'has generated a source', function () {
 		expect( parserSource ).to.be.an.object;
@@ -18,12 +26,23 @@ describe.only( 'Parser', () => {
 
 	describe( 'should parse successfully the given expressions:', () => {
 
-		expressions.forEach( item => {
+		positiveTests.forEach( item => {
 			if ( item && !_.isEmpty( item ) ) {
 				it( item, function () {
 					item = _.trimRight( item, '\r' );
 					var r = parser.parse( item );
 					expect( r ).to.equal( item );
+				} );
+			}
+		} );
+	} );
+
+	describe( 'should successfully fail to parse', () => {
+		negativeTests.forEach( item => {
+			if ( item && !_.isEmpty( item ) ) {
+				it( item, function () {
+					item = _.trimRight( item, '\r' );
+					expect(parser.parse.bind(null, item)).to.throw(/*whatever*/);
 				} );
 			}
 		} );
